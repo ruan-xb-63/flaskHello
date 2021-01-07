26ac6c5afdb3cbcc03f054a55efe01bef0d586ee
阮贤彪 <1309539970@qq.com>
2021-01-07 10:49:10 +0800

    hello,world

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/<name>")
+def print_hi(name):
+    serverSay = f'Hi, {name}'
+    return serverSay
+
+
+if __name__ == '__main__':
+    app.run()
